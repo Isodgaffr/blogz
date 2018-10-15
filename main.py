eee8159cf0e59a7e502d4a61951fa3ff4644bda1
Gibraun Mitchell <isobloodbank@Gibrauns-MacBook-Pro.local>
2018-10-15 16:47:40 -0500

    Working

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URL'] = 'mysql+pymysql://build-a-blog:build-a-blog@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
