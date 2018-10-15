076dfb7bf8e18a07a0959ad56b9cfb8ef95de602
Gibraun Mitchell <isobloodbank@Gibrauns-MacBook-Pro.local>
2018-10-15 18:47:42 -0500

    Completed Assignment

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
+app.secret_key = 'aidbojhbgzisfoghsfghioffusw'
 
 
 
